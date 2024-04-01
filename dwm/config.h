d0d7e931b922ecc17de02c6beb6f5f6c765f5dd5
obrien-reece <indeche.evans.o@gmail.com>
2024-04-01 20:45:49 +0300

    neofetch

--- dwm/config.h
+++ dwm/config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "/home/obrien/Downloads/dotfiles/dwm/themes/green.h"
+#include "/home/indeche/Downloads/dotfiles/dwm/themes/purple.h"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
