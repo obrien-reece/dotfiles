08cb988b61584ed1decd85180ee7e3d30bd26b4b
obrien-reece <indeche.evans.o@gmail.com>
2024-10-01 13:17:51 +0300

    added configurations to ufo folding

--- nvim/lua/ufo-nvim/init.lua
+++ nvim/lua/ufo-nvim/init.lua
@@ -1,7 +1,10 @@
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+require('ufo').setup()
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
@@ -15,8 +18,21 @@ vim.keymap.set('n', 'zK', function()
 end)
 
 require('ufo').setup({
-  provider_selector = function(bufnr, filetype, buftype)
-    return { 'lsp', 'indent' }
-  end
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
 })
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true
+}
+
+local language_servers = {'lua_ls', 'elmls', 'gopls'}
+for _, ls in ipairs(language_servers) do
+    require('lspconfig')[ls].setup({
+        capabilities = capabilities
+        -- you can add other fields for setting up lsp server in this table
+    })
+end
